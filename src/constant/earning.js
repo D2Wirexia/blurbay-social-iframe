ffdfa47913e200bf7aaa4b404e75c686199f4e2a
Wirexia <dmitryw1seschenko@ukr.net>
2024-07-10 14:58:22 +0300

    #008

--- src/constant/earning.js
+++ src/constant/earning.js
@@ -4,84 +4,88 @@ import * as ENGAGEMENT_RATE from './engagement-rate'
 export const Earning = {
     [ENGAGEMENT_RATE.Low]: {
         [SOCIALS.Youtube]: {
-            min: 11,
-            max: 44,
+            min: 42,
+            max: 601,
         },
         [SOCIALS.Facebook]: {
-            min: 11,
-            max: 44,
+            min: 40,
+            max: 444,
         },
         [SOCIALS.Twitter]: {
-            min: 11,
-            max: 44,
+            min: .5,
+            max: 2,
         },
         [SOCIALS.Instagram]: {
-            min: 11,
-            max: 44,
+            min: 43,
+            max: 533,
         },
         [SOCIALS.TikTok]: {
-            min: 11,
-            max: 44,
+            min: 29,
+            max: 311,
         },
         [SOCIALS.Blurbay]: {
-            min: 11,
-            max: 44,
+            min: 5012,
+            max: 9025,
         },
     },
     [ENGAGEMENT_RATE.Average]: {
         [SOCIALS.Youtube]: {
-            min: 11,
-            max: 44,
+            min: 95,
+            max: 1352,
         },
         [SOCIALS.Facebook]: {
-            min: 11,
-            max: 44,
+            min: 90,
+            max: 1000,
         },
         [SOCIALS.Twitter]: {
-            min: 11,
-            max: 44,
+            min: 1,
+            max: 4,
         },
         [SOCIALS.Instagram]: {
-            min: 11,
-            max: 44,
+            min: 96,
+            max: 1200,
         },
         [SOCIALS.TikTok]: {
-            min: 11,
-            max: 44,
+            min: 66,
+            max: 700,
         },
         [SOCIALS.Blurbay]: {
-            min: 11,
-            max: 44,
+            min: 11277,
+            max: 20305,
         },
     },
     [ENGAGEMENT_RATE.High]: {
         [SOCIALS.Youtube]: {
-            min: 11,
-            max: 44,
+            min: 211,
+            max: 3001,
         },
         [SOCIALS.Facebook]: {
-            min: 11,
-            max: 44,
+            min: 200,
+            max: 2220,
         },
         [SOCIALS.Twitter]: {
-            min: 11,
-            max: 44,
+            min: 2,
+            max: 9,
         },
         [SOCIALS.Instagram]: {
-            min: 11,
-            max: 44,
+            min: 213,
+            max: 2664,
         },
         [SOCIALS.TikTok]: {
-            min: 11,
-            max: 44,
+            min: 147,
+            max: 1554,
         },
         [SOCIALS.Blurbay]: {
-            min: 11,
-            max: 44,
+            min: 25059,
+            max: 45123,
         },
     },
 }
 
 export const buildEarning = (val, c) => {
-    return `$${val.min * c}-$${val.max * c}`
+    return formatNumber(val.min * c) + '-' + formatNumber(val.max * c)
+}
+
+const formatNumber = (number) => {
+    return '$' + number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
